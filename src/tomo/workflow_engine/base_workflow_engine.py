4aa9f262a31a778a278a93b9dea2dd149151b4ce
Shuai ZHANG <dyzsasd@gmail.com>
2024-10-15 16:59:32 +0200

    REFAC: rename policy to workflwo

--- src/tomo/workflow_engine/base_workflow_engine.py
+++ src/tomo/workflow_engine/base_workflow_engine.py
@@ -1,4 +1,4 @@
-class PolicyEngine:
+class WorkflowEngine:
     def decide_action(self, intent, entities, session_data):
         # Based on the extracted intent and entities, determine next action
         pass
